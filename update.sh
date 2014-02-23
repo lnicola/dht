1335bcd31e9616c153e083f1cfa82d528a319ff4
Laurentiu Nicola <lnicola@dend.ro>
2014-02-23 20:18:19 +0200

    Include current temperature and humidity in web page

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ do
     
     if [ ${#FOO[@]} -eq 2 ]; then
         rrdtool update temperature.rrd N:${FOO[0]}:${FOO[1]}
+
+        sed -i "s/<div id=\"data\">.*<\/div>/<div id=\"data\">${FOO[0]} °C ${FOO[1]}%<\/div>/" temperature.html
     fi
 
     sleep 5
